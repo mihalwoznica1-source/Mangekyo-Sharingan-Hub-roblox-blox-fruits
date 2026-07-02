94ce89e9c4373ff1edab9e15e5cd79e36ef68cea
mihalwoznica1-source <mihal.woznica1@gmail.com>
2026-07-03 01:21:44 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,14 +1,3 @@
--- === Mangekyo Sharingan Hub - Main ===
-print("🔴 Uruchamiam Mangekyo Sharingan Hub...")
-
-local repo = "https://raw.githubusercontent.com/mihalwoznicai-source/Mangekyo-Sharingan-Hub-roblox-blox-fruits/main/"
-
--- Najpierw Intro
-loadstring(game:HttpGet(repo .. "Intro.lua"))()
-
-wait(5)  -- czekamy aż intro się skończy
-
--- Potem Hub
-loadstring(game:HttpGet(repo .. "Hub.lua"))()
-
-print("Hub powinien się pojawić!")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mihalwoznicai-source/Mangekyo-Sharingan-Hub-roblox-blox-fruits/main/Intro.lua"))()
+wait(5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mihalwoznicai-source/Mangekyo-Sharingan-Hub-roblox-blox-fruits/main/Hub.lua"))()
