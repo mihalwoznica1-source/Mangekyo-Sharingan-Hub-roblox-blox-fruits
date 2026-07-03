890f8989894a61773d8e53500c89760c3e72ae78
mihalwoznica1-source <mihal.woznica1@gmail.com>
2026-07-03 02:11:41 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/mihalwoznicai-source/Mangekyo-Sharingan-Hub-roblox-blox-fruits/main/Intro.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mihalwoznica1-source/Mangekyo-Sharingan-Hub-roblox-blox-fruits/main/Intro.lua"))()
 wait(5)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/mihalwoznicai-source/Mangekyo-Sharingan-Hub-roblox-blox-fruits/main/Hub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mihalwoznica1-source/Mangekyo-Sharingan-Hub-roblox-blox-fruits/main/Hub.lua"))()
